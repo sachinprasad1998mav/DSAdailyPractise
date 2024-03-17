1d01e43c7da727c1b68c835508c924951748335d
Sachin <sachinprasad1998mav@gmail.com>
2024-03-17 16:30:15 -0500

    contains duplicate completed

---
+++ Arrays & Hashing/containsDuplicate.py
@@ -0,0 +1,33 @@
+# Contains Duplicate
+# Easy
+
+
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+
+# Algorithm : 
+# Create a set and add nums if not in set.
+
+
+def containsDuplicate(nums) -> bool:
+    s = set()
+    for i in nums:
+        if i in s:
+            return True
+        s.add(i)
+    return False
